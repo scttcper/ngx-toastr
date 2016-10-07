cf27907a2651ab23037bac3ce10555f6cf946939
Scott Cooper <scttcper@gmail.com>
2016-10-07 00:32:04 -0700

    remove http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ToastrModule } from '../lib/toastr';
@@ -13,7 +12,6 @@ import { ToastrModule } from '../lib/toastr';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
     ToastrModule,
   ],
   providers: [],
