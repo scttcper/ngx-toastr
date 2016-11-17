4d843729eb31b29964a9ca35671dc7e8003c3dd8
Scott Cooper <scttcper@gmail.com>
2016-11-17 12:37:51 -0800

    bump cli version (#23)
    * upgrade cli
    
    * don't publish sourcemaps and ngfactory files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,7 +3,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('App: ToastrNg2', () => {
+describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
