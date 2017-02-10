90edef2d72fa5e68e23b787fc754139bf57f223f
Scott Cooper <scttcper@gmail.com>
2017-02-09 20:26:00 -0800

    change how github pages works

--- pages.sh
+++ pages.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ $TRAVIS_BRANCH != 'master' ]]; then
+	# Not master branch aborting
+	exit 0
+fi
 yarn build
 yarn global add gh-pages
-gh-pages -r https://$PUSH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git -d dist
+echo "Pushing to github pages"
+gh-pages -r "https://$PUSH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" -d dist
+git clone https://github.com/scttcper/angular-cli.git
