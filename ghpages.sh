349691cbcf14a7bae76c670eef2cb1faf46f1647
Scott Cooper <scttcper@gmail.com>
2017-02-09 20:50:41 -0800

    fix baseurl

--- ghpages.sh
+++ ghpages.sh
@@ -4,7 +4,7 @@ if [[ $TRAVIS_BRANCH != 'master' ]]; then
 	# Not master branch aborting
 	exit 0
 fi
-yarn build
+yarn ghpages
 yarn global add gh-pages
 echo "Pushing to github pages"
 gh-pages -r "https://$PUSH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" -d dist -x
