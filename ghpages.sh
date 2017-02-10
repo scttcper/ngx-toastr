fbbf180e004f23196f157500c9df53fdc8cddfdb
Scott Cooper <scttcper@gmail.com>
2017-02-09 20:35:21 -0800

    silent github pulling

--- ghpages.sh
+++ ghpages.sh
@@ -7,5 +7,5 @@ fi
 yarn build
 yarn global add gh-pages
 echo "Pushing to github pages"
-gh-pages -r "https://$PUSH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" -d dist
+gh-pages -r "https://$PUSH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" -d dist -x
 git clone https://github.com/scttcper/angular-cli.git
