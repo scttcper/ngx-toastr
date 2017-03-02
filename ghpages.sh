7d1e26089a3c41f3c08aaff3a9eae5f95eaf5dd2
Scott Cooper <scttcper@gmail.com>
2017-03-02 11:19:12 -0800

    remove cache and broken yarn update

--- ghpages.sh
+++ ghpages.sh
@@ -2,9 +2,15 @@
 
 if [[ $TRAVIS_BRANCH != 'master' ]]; then
 	# Not master branch aborting
+  echo "this is not master"
 	exit 0
 fi
+echo "creating build"
 yarn ghpages
+echo "finished build"
+git config --global user.email "$PUSH_EMAIL"
+git config --global user.name "Travis CI"
+git config --global push.default simple
 yarn global add gh-pages
 echo "Pushing to github pages"
 gh-pages -r "https://$PUSH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" -d dist -x
