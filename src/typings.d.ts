b37411b1c1f0c57784c7045a7a0a109fff90e67f
Scott Cooper <scttcper@gmail.com>
2017-03-06 14:19:11 -0800

    upgrade angular-cli, new split tsconfigs

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,2 +1,4 @@
-// Typings reference file, you can add your own global typings here
-// https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
+/* SystemJS module definition */
+declare var module: {
+  id: string;
+};
