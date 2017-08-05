a8ad380a5fbd80628ffd6f345efb7a78bb6bc17b
Scott Cooper <scttcper@gmail.com>
2017-08-05 15:40:33 -0700

    add web-animations polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -46,7 +46,7 @@ import 'core-js/es7/reflect';
  * Required to support Web Animations `@angular/animation`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
  **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
 
