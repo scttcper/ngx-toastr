59b2c0d61535f230627f891875aeab551fc1a1a3
Scott Cooper <scttcper@gmail.com>
2017-11-28 16:35:19 -0800

    allow on action multiple times closes #273

--- src/lib/toastr/toastr-config.ts
+++ src/lib/toastr/toastr-config.ts
@@ -130,8 +130,8 @@ export interface GlobalConfig extends IndividualConfig {
  * Everything a toast needs to launch
  */
 export class ToastPackage {
-  private _onTap: Subject<any> = new Subject();
-  private _onAction: Subject<any> = new Subject();
+  private _onTap = new Subject<any>();
+  private _onAction = new Subject<any>();
 
   constructor(
     public toastId: number,
@@ -140,7 +140,11 @@ export class ToastPackage {
     public title: string | undefined,
     public toastType: string,
     public toastRef: ToastRef<any>,
-  ) { }
+  ) {
+    this.toastRef.afterClosed().subscribe(() => {
+      this._onAction.complete();
+    });
+  }
 
   /** Fired on click */
   triggerTap() {
@@ -155,13 +159,11 @@ export class ToastPackage {
   /** available for use in custom toast */
   triggerAction(action?: any) {
     this._onAction.next(action);
-    this._onAction.complete();
   }
 
   onAction(): Observable<any> {
     return this._onAction.asObservable();
   }
-
 }
 
 /* tslint:disable:no-empty-interface */
