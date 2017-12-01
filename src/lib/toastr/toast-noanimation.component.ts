87da9bd001854ca042db8e36430bb187d8ce8171
Scott Cooper <scttcper@gmail.com>
2017-11-30 22:46:03 -0800

    fix progress on noanimation component

--- src/lib/toastr/toast-noanimation.component.ts
+++ src/lib/toastr/toast-noanimation.component.ts
@@ -161,7 +161,7 @@ export class ToastNoAnimation implements OnDestroy {
     );
     this.options.timeOut = this.options.extendedTimeOut;
     this.hideTime = new Date().getTime() + (this.options.timeOut || 0);
-    this.width = 100;
+    this.width = -1;
     if (this.options.progressBar) {
       this.intervalId = setInterval(() => this.updateProgress(), 10);
     }
