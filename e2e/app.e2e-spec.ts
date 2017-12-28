d7f0b02a3cefade58d60d11059374afdee218401
PhilippStein <git@philipp-stein.de>
2017-12-29 00:36:05 +0100

    e2e(ngZone): test that toasts do not block protractor execution (#109)

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -17,4 +17,26 @@ describe('toastr App', () => {
       expect<any>(page.getToast().getText()).toEqual('HELLO THERE');
     });
   });
+
+  it('should not block protractor while toast is visible', () => {
+      browser.waitForAngularEnabled(true);
+      page.navigateTo();
+      page.enterMessage();
+      page.clickShowToast();
+      // No need to disable `waitForAngularEnabled´
+      page.waitForToast().then((element) => {
+          expect<any>(page.getToast().getText()).toEqual('HELLO THERE');
+      });
+      // Test does not run in a timeout
+  });
+
+  it('should auto close the toast within the timeout', () => {
+      browser.waitForAngularEnabled(true);
+      page.navigateTo();
+      page.setTimeout(100);
+      page.clickShowToast();
+      page.waitForToast().then((element) => {
+        page.waitForToastDisappear(400); // timeout + ease time
+      });
+    });
 });
