31a3cd15556977d22b96de346a8773d9139a251a
Scott Cooper <scttcper@gmail.com>
2018-02-05 23:32:52 -0800

    switch build process to ng-packagr

--- build.ts
+++ build.ts
@@ -1,191 +1,39 @@
-/* tslint:disable:import-blacklist */
-// from https://github.com/angular/angularfire2/blob/master/tools/build.js
-import { spawn } from 'child_process';
-import * as copyfiles from 'copy';
-import { copy } from 'fs-extra';
-import { rollup } from 'rollup';
-import * as filesize from 'rollup-plugin-filesize';
-import * as sourcemaps from 'rollup-plugin-sourcemaps';
-import { Observable } from 'rxjs';
+import { copySync } from 'fs-extra';
+import { build } from 'ng-packagr';
+import { join } from 'path';
+import * as rimraf from 'rimraf';
 
-const pkg = require(`${process.cwd()}/package.json`);
+async function main() {
+  // cleanup dist
+  rimraf.sync(join(process.cwd(), '/dist'));
 
-// Rollup globals
-const GLOBALS = {
-  '@angular/core': 'ng.core',
-  '@angular/common': 'ng.common',
-  '@angular/animations': 'ng.animations',
-  '@angular/platform-browser': 'ng.platformBrowser',
-  'rxjs/Observable': 'Rx',
-  'rxjs/Subject': 'Rx',
-  'rxjs/Observer': 'Rx',
-  'rxjs/Subscription': 'Rx',
-};
-
-// Constants for running typescript commands
-const NGC = './node_modules/.bin/ngc';
-const TSC_ARGS = (config = 'build') => [
-  `-p`,
-  `${process.cwd()}/src/lib/tsconfig-${config}.json`,
-];
-
-/**
- * Create an Observable of a spawned child process.
- * @param {string} command
- * @param {string[]} args
- */
-function spawnObservable(command, args) {
-  return Observable.create(observer => {
-    const cmd = spawn(command, args);
-    observer.next(''); // hack to kick things off, not every command will have a stdout
-    cmd.stdout.on('data', data => {
-      observer.next(data.toString());
-    });
-    cmd.stderr.on('data', data => {
-      observer.error(data.toString());
-    });
-    cmd.on('close', data => {
-      observer.complete();
-    });
+  // build packages
+  await build({
+    project: join(process.cwd(), 'src/lib/package.json'),
   });
+
+  // copy everything else
+  copySync('README.md', join(process.cwd(), 'dist/README.md'));
+  copySync('LICENSE', join(process.cwd(), 'dist/LICENSE'));
+  copySync(
+    join(process.cwd(), 'src/lib/toastr-bs4-alert.scss'),
+    join(process.cwd(), 'dist/toastr-bs4-alert.scss'),
+  );
+  copySync(
+    join(process.cwd(), 'src/lib/toastr.css'),
+    join(process.cwd(), 'dist/toastr.css'),
+  );
+  copySync(
+    join(process.cwd(), 'src/lib/toastr-old.css'),
+    join(process.cwd(), 'dist/toastr-old.css'),
+  );
 }
 
-function generateBundle(input, file, globals, name, format) {
-  const plugins = [sourcemaps(), filesize()];
-  return rollup({
-    input,
-    external: Object.keys(globals),
-    file,
-    plugins,
-    onwarn(warning) {
-      if (warning.code === 'THIS_IS_UNDEFINED') {
-        return;
-      }
-      if (warning.code === 'UNUSED_EXTERNAL_IMPORT') {
-        return;
-      }
-      console.log(warning.message);
-    },
-  }).then(bundle => {
-    return bundle.write({
-      file,
-      name,
-      globals,
-      format,
-      sourcemap: true,
-    });
+main()
+  .then(() => console.log('success'))
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
   });
-}
 
-function createUmd(globals) {
-  const name = 'ngx-toastr';
-  const entry = `${process.cwd()}/dist/es5/index.js`;
-  return generateBundle(
-    entry,
-    `${process.cwd()}/dist/packages-dist/toastr.umd.js`,
-    globals,
-    name,
-    'umd',
-  );
-}
 
-function createEs(globals, target) {
-  const name = 'ngx-toastr';
-  const entry = `${process.cwd()}/dist/${target}/index.js`;
-  return generateBundle(
-    entry,
-    `${process.cwd()}/dist/packages-dist/toastr.${target}.js`,
-    globals,
-    name,
-    'es',
-  );
-}
-
-function getVersions() {
-  const paths = [`${process.cwd()}/dist/packages-dist/package.json`];
-  return paths.map(path => require(path)).map(pkgs => pkgs.version);
-}
-
-function verifyVersions() {
-  const versions = getVersions();
-  console.log(versions);
-  versions.map(version => {
-    if (version !== pkg.version) {
-      throw new Error('Versions mistmatch');
-    }
-  });
-}
-
-function buildModule(globals) {
-  const es2015$ = spawnObservable(NGC, TSC_ARGS());
-  const esm$ = spawnObservable(NGC, TSC_ARGS('esm'));
-  return Observable.forkJoin(es2015$, esm$);
-}
-
-function createBundles(globals) {
-  return Observable.forkJoin(
-    Observable.from(createUmd(globals)),
-    Observable.from(createEs(globals, 'es2015')),
-    Observable.from(createEs(globals, 'es5')),
-  );
-}
-
-function copyFiles() {
-  const copyAll: ((s: string, s1: string) => any) = Observable.bindCallback(
-    copyfiles,
-  );
-  return Observable.forkJoin(
-    copyAll(
-      `${process.cwd()}/dist/es2015/**/*.d.ts`,
-      `${process.cwd()}/dist/packages-dist`,
-    ),
-    copyAll(
-      `${process.cwd()}/dist/es2015/**/*.metadata.json`,
-      `${process.cwd()}/dist/packages-dist`,
-    ),
-    Observable.from(
-      copy(
-        `${process.cwd()}/README.md`,
-        `${process.cwd()}/dist/packages-dist/README.md`,
-      ),
-    ),
-    Observable.from(
-      copy(
-        `${process.cwd()}/src/lib/package.json`,
-        `${process.cwd()}/dist/packages-dist/package.json`,
-      ),
-    ),
-    Observable.from(
-      copy(
-        `${process.cwd()}/src/lib/toastr.css`,
-        `${process.cwd()}/dist/packages-dist/toastr.css`,
-      ),
-    ),
-    Observable.from(
-      copy(
-        `${process.cwd()}/src/lib/toastr-old.css`,
-        `${process.cwd()}/dist/packages-dist/toastr-old.css`,
-      ),
-    ),
-    Observable.from(
-      copy(
-        `${process.cwd()}/src/lib/toastr-bs4-alert.scss`,
-        `${process.cwd()}/dist/packages-dist/toastr-bs4-alert.scss`,
-      ),
-    ),
-  );
-}
-
-function buildLibrary(globals) {
-  const modules$ = buildModule(globals);
-  return Observable.forkJoin(modules$)
-    .switchMap(() => createBundles(globals))
-    .switchMap(() => copyFiles())
-    .do(() => verifyVersions());
-}
-
-buildLibrary(GLOBALS).subscribe(
-  data => console.log('success'),
-  err => console.log('err', err),
-  () => console.log('complete'),
-);
