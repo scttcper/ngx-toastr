eee8b419fbcceaac2ce86e96f078e861ed500316
Scott Cooper <scttcper@gmail.com>
2018-08-13 21:34:30 -0700

    chore: upgrade angular files, add semantic release (#535)
    * chore: upgrade angular files, add semantic release
    
    * whoops
    
    * docs: add github button instead of large github svg
    
    * build: fix ts settings
    
    * revert tslint changes
    
    * revert change to karma styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,11 @@
 import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-
-import json from '../lib/package.json';
-
 
 @Component({
   selector: 'app-root',
   template: `
   <app-header></app-header>
-  <github-link></github-link>
   <app-home></app-home>
   <app-footer></app-footer>
 `,
 })
-export class AppComponent {
-  constructor(title: Title) {
-    // sync options to toastrservice
-    // this sets the options in the demo
-    const current = title.getTitle();
-    // fix for tests
-    if (json) {
-      title.setTitle(`${current}: v${json.version}`);
-    }
-  }
-}
+export class AppComponent {}
