314b769de3c98055e71786d912eb5ee171804a3e
Joan-Angelo Douvere <angelo.douvere@gmail.com>
2018-08-17 16:16:29 +0200

    activate countDuplicates by default

--- src/lib/toastr/toastr-config.ts
+++ src/lib/toastr/toastr-config.ts
@@ -198,7 +198,7 @@ export const DefaultNoComponentGlobalConfig: GlobalConfig = {
   autoDismiss: false,
   newestOnTop: true,
   preventDuplicates: false,
-  countDuplicates: false,
+  countDuplicates: true,
   resetTimeoutOnDuplicate: false,
   iconClasses: {
     error: 'toast-error',
