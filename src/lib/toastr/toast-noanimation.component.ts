9c890e61a7a5efd9bdc566ccde9f5aa455a565dd
Bram Eikelboom <bram.eikelboom@profects.com>
2019-10-16 16:30:55 +0200

    fix: timeOut is not disabled by `disableTimeOut: true` in ToastNoAnimation (#724)

--- src/lib/toastr/toast-noanimation.component.ts
+++ src/lib/toastr/toast-noanimation.component.ts
@@ -109,7 +109,7 @@ export class ToastNoAnimation implements OnDestroy {
    */
   activateToast() {
     this.state = 'active';
-    if ((this.options.disableTimeOut === false || this.options.disableTimeOut !== 'timeOut') && this.options.timeOut) {
+    if (!(this.options.disableTimeOut === true || this.options.disableTimeOut === 'timeOut') && this.options.timeOut) {
       this.timeout = setTimeout(() => {
         this.remove();
       }, this.options.timeOut);
