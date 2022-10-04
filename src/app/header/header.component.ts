ba0d89a35c677fd7d29540077646ccec8c9018dd
Rajeev Kumar <rajeev@i2vsys.com>
2022-10-04 23:20:36 +0530

    make header and footer interactive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   template: `
-    <header class="header mt-5 text-center">
-      <h1>Angular Toastr</h1>
-      <p style="color: #777" class="mb-1">Easy Toasts for Angular</p>
+    <header class="header text-center">
+      <div style="background-color: #1162D8;color:white;padding-bottom:2px;">
+      <h1 >Angular Toastr</h1>
+      </div>
+      <p style="color: #777" class="mb-1 mt-5">Easy Toasts for Angular</p>
       <gh-button user="scttcper" repo="ngx-toastr" [count]="true"></gh-button>
     </header>
   `,
